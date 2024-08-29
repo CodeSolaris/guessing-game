41eae85e8b447dd8eee9268be47752429a6a6e1f
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-08-30 00:45:52 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0; // Добавляем переменную для хранения текущей гипотезы
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.round((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess; // Используем текущую гипотезу
     }
 
     greater() {
-
+        this.min = this.currentGuess; // Используем текущую гипотезу
     }
 }
 
